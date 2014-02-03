330f99931410301ef9c2d5bb1072d1f5b6c7a180
chrisbodhi <cboette@gmail.com>
2014-02-03 00:27:35 -0600

    Move to test straight-up Google Maps example code after numerous head-scratching non-actions involving jQuery and a button.

---
+++ main.js
@@ -0,0 +1,32 @@
+// var map;
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 8,
+//     center: new google.maps.LatLng(-34.397, 150.644)
+//   };
+//   map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
+
+$(document).on('load', function () {
+  console.log("you got this.");
+});
+
+$("#clicker").click(function () {
+  console.log("Don't fuck it up.");
+});
+
+// $('button').on('click', function () {
+//   navigator.geolocation.getCurrentPosition(
+//     function(position) {
+//       alert("You were last spotted at (" + position.coords.latitude + "," + position.coords.longitude + ")");
+//       var lat1 = position.coords.latitude;//.toString().replace('.', 'x');
+//       var long1 = position.coords.longitude;//.toString().replace('.', 'x');
+//       codes = [lat1, long1];
+//       console.log(codes);
+//       initialize();
+//       return codes;
+//   });
+// });
